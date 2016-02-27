1b4cb449e3dcf2562d19806db3928f5b14b4bf88
domanhchat <domanhchat@gmail.com>
2016-02-27 15:30:00 +0700

    add comment

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,7 +6,9 @@ use App\Classes\PublicController;
 
 class IndexController extends PublicController
 {
-    
+    /**
+     * Home action
+     */
     public function index()
     {
     	return view('home');
