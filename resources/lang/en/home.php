a257ebfe57b5dc3d8577dbb2cae1cfff87a77146
domanhchat <domanhchat@gmail.com>
2016-02-27 17:05:32 +0700

    add file

---
+++ resources/lang/en/home.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'dang_tin_free' => 'Đăng tin miễn phí_EN',
+	'ban_lai_sologan' => 'Bán hoặc quảng cáo_EN   <span class="segment-heading">    trực tuyến bất kỳ đâu </span> với Banlai.vn',
+	'ban_lai_header_sologan' => 'Trang thông tin rao vặt hàng đầu Việt Nam_EN',
+	
+];
---
+++ resources/lang/vi/home.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'dang_tin_free' => 'Đăng tin miễn phí',
+	'ban_lai_sologan' => 'Bán hoặc quảng cáo   <span class="segment-heading">    trực tuyến bất kỳ đâu </span> với Banlai.vn',
+	'ban_lai_header_sologan' => 'Trang thông tin rao vặt hàng đầu Việt Nam',
+	
+];
