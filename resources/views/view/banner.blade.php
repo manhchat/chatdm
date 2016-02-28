c1648373d462cbb67117f4fb8a5c65ac7f611cfe
domanhchat <domanhchat@gmail.com>
2016-02-28 21:37:14 +0700

    sdsds

--- resources/views/view/banner.blade.php
+++ resources/views/view/banner.blade.php
@@ -2,6 +2,6 @@
   <div class="container">    
 		<h1><?php echo trans('home.ban_lai_sologan')?></h1>
 		<p><?php echo trans('home.ban_lai_header_sologan')?></p>
-		<a href="post-ad.html"><?php echo trans('home.dang_tin_free')?></a>
+		<a href="<?php echo url('dang-tin')?>"><?php echo trans('home.dang_tin_free')?></a>
   </div>
 </div>
