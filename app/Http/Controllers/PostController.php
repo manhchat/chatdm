17b89bbf89f33b8e4e5db6353e39cc2b7bbb3998
manhchat <domanhchat@gmail.com>
2016-03-07 14:16:21 +0700

    commit style

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,7 @@ use Session;
 use App\Classes\Codedef;
 use App\Classes\Func;
 use Illuminate\Support\Facades\Input;
+use Validator;
 class PostController extends PublicController
 {
     /**
@@ -39,9 +40,33 @@ class PostController extends PublicController
     
     public function create()
     {
-    	echo "<pre>";
-    	var_dump(Input::all());
-    	var_dump(Input::file());
+    	$rules = array(
+    			'category'            => 'required',     // required and must be unique in the ducks table
+    			'title'         => 'required',
+    			'description' => 'required',
+    			'price' => 'required',
+    			'name' => 'required',
+    			'phone' => 'required',
+    			'email' => 'required|email'
+    	);
+    	$messages = array(
+    			'category.required' => 'Hãy chọn danh mục rao vặt.',
+    			'title.required' => 'Hãy nhập tiêu đề.',
+    			'description.required' => 'Hãy nhập nội dung rao vặt.',
+    			'price.required' => 'Hãy nhập giá.',
+    			'name.required' => 'Hãy nhập tên của bạn.',
+    			'phone.required' => 'Hãy nhập số điện thoại của bạn.',
+    			'email.required' => 'Hãy nhập số email của bạn.',
+    			'email.email' => 'Địa chỉ email của bạn không đúng.'
+    	);
+    	$validator = Validator::make(Input::all(),$rules, $messages);
+    	if ($validator->fails()) {
+    		// get the error messages from the validator
+    		$messages = $validator->messages();
+    		// redirect our user back to the form with the errors from the validator
+    		return redirect('dang-tin')->withErrors($validator)->withInput();
+    	} else {
+    	}
     }
 
 }
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -14,18 +14,27 @@
 	<div class="container">
 		<h2 class="head"><?php echo trans('post.create_title')?></h2>
 		<div class="post-ad-form">
+			<?php $classError = ''?>
+			@if ($errors->has())
+			<?php $classError = ' error'?>
+		        <div class="alert alert-danger">
+		            @foreach ($errors->all() as $error)
+		                {{ $error }}<br>        
+		            @endforeach
+		        </div>
+	        @endif
 			<?php echo Form::open(array('url' => url('post/create')))?>
 				<label class="post-label"><?php echo trans('post.select_category')?> <span>*</span></label>
-				<?php echo Form::select('category', $listCategory,'', array('id' => 'category', 'class' => 'selectpicker', 'data-live-search' => 'true', 'data-style' => 'btn-success', 'data-dropup-auto' => 'false'))?>
+				<?php echo Form::select('category', $listCategory, old('category'), array('id' => 'category', 'class' => 'selectpicker', 'data-live-search' => 'true', 'data-style' => 'btn-success', 'data-dropup-auto' => 'false'))?>
 				<div class="clearfix"></div>
 				<label class="post-label"><?php echo trans('post.create_title_of_ad')?> <span>*</span></label>
-				<?php echo Form::text('title', old('title'), array('class' => 'phone post-text', 'placeholder' => trans('post.create_title_placeholder')))?>
+				<?php echo Form::text('title', old('title'), array('class' => 'phone post-text'. $errors->first('title', ' error'), 'placeholder' => trans('post.create_title_placeholder')))?>
 				<div class="clearfix"></div>
 				<label class="post-label"><?php echo trans('post.create_description_of_ad')?> <span>*</span></label>
-				<?php echo Form::textarea('description', old('description'), array('class' => 'mess', 'id' => 'ckeditor_area', 'placeholder' => trans('post.create_description_placeholder')))?>
+				<?php echo Form::textarea('description', old('description'), array('class' => 'mess'.$classError, 'id' => 'ckeditor_area', 'placeholder' => trans('post.create_description_placeholder')))?>
 				<div class="clearfix"></div>
 				<label class="post-label"><?php echo trans('post.create_price')?> <span>*</span></label>
-				<?php echo Form::text('price', old('price'), array('class' => 'price post-text', 'placeholder' => trans('post.create_price')))?>
+				<?php echo Form::text('price', old('price'), array('class' => 'price post-text'. $errors->first('price', ' error'), 'placeholder' => trans('post.create_price')))?>
 				<div class="clearfix"></div>
 				<div class="upload-ad-photos">
 				<label class="post-label"><?php echo trans('post.create_image')?> :</label>	
@@ -37,13 +46,13 @@
 				</div>
 			<div class="personal-details">
 				<label class="post-label"><?php echo trans('post.create_your_name')?> <span>*</span></label>
-				<?php echo Form::text('name', old('name'), array('class' => 'name post-text', 'placeholder' => trans('post.create_your_name_placeholder')))?>
+				<?php echo Form::text('name', old('name'), array('class' => 'name post-text'. $errors->first('name', ' error'), 'placeholder' => trans('post.create_your_name_placeholder')))?>
 				<div class="clearfix"></div>
 				<label class="post-label"><?php echo trans('post.create_your_mobile')?> <span>*</span></label>
-				<?php echo Form::text('name', old('phone'), array('class' => 'phone post-text', 'placeholder' => trans('post.create_your_phone_placeholder')))?>
+				<?php echo Form::text('phone', old('phone'), array('class' => 'phone post-text'. $errors->first('phone', ' error'), 'placeholder' => trans('post.create_your_phone_placeholder')))?>
 				<div class="clearfix"></div>
 				<label class="post-label"><?php echo trans('post.create_your_email')?><span>*</span></label>
-				<?php echo Form::text('name', old('email'), array('class' => 'email post-text', 'placeholder' => trans('post.create_your_email_placeholder')))?>
+				<?php echo Form::text('email', old('email'), array('class' => 'email post-text'. $errors->first('email', ' error'), 'placeholder' => trans('post.create_your_email_placeholder')))?>
 				<div class="clearfix"></div>
 				<p class="post-terms"><?php echo trans('post.create_term')?></p>
 				<input type="submit" value="<?php echo trans('post.create')?>">			
